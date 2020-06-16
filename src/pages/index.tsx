c56997a0487dd22193aba1f57df0a105b5e04b83
Artur Siery <artur.siery@intive.com>
2020-06-16 10:03:47 +0200

    Reading sync data from FS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import { getAllPosts } from "../utils/post-reader";
+
 const Home = () => {
   return <h1>Home</h1>;
 };
+
+export async function getServerSideProps() {
+  const posts = getAllPosts();
+  console.log(posts);
+  return { props: { data: "test", posts } };
+}
 
 export default Home;
---
+++ src/utils/post-reader.tsx
@@ -0,0 +1,49 @@
+import fs from "fs";
+import fm from "front-matter";
+
+const postDir = process.cwd() + "/data/posts";
+
+interface ParsedPost {
+  attributes: {
+    date: Date;
+    title: string;
+    tags: string[];
+  };
+  body: string;
+}
+
+interface Post {
+  attributes: {
+    date: number;
+    title: string;
+    tags: string[];
+  };
+  body: string;
+}
+
+const parsePost = (data: string): Post => {
+  const parsed = fm(data) as ParsedPost;
+
+  const post: Post = {
+    attributes: {
+      date: Math.round(parsed.attributes.date.valueOf() / 1000),
+      title: parsed.attributes.title,
+      tags: parsed.attributes.tags,
+    },
+    body: parsed.body,
+  };
+
+  return post;
+};
+
+const readPostsFromFiles = () => {
+  const posts = fs.readdirSync(postDir, "utf-8");
+  const firstPost = fs.readFileSync(postDir + "/" + posts[0], "utf-8");
+  const post = parsePost(firstPost);
+
+  return [post];
+};
+
+export const getAllPosts = () => {
+  return readPostsFromFiles();
+};
