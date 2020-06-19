2acad06ac44b9d7908cbf93afb19995f4b88f6eb
Artur Siery <artur.siery@intive.com>
2020-06-19 11:06:01 +0200

    Using dir in api for image

--- src/pages/api/img/[dir]/[name].tsx
+++ src/pages/api/img/[dir]/[name].tsx
@@ -7,9 +7,9 @@ const readFile = util.promisify(fs.readFile);
 const imgDir = process.cwd() + "/data/img/";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const { name } = req.query;
+  const { dir, name } = req.query;
 
-  readFile(imgDir + name)
+  readFile(imgDir + dir + "/" + name)
     .then((data) => {
       res.statusCode = 200;
       res.setHeader("Content-Type", "image/jpeg");
